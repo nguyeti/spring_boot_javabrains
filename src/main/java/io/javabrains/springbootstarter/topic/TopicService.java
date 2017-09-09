835bbed3613204401d83091488ac5bee0a7773e3
Timothée Nguyen <nguyen.timothee@gmail.com>
2017-09-09 12:07:16 -0400

    first commit

---
+++ src/main/java/io/javabrains/springbootstarter/topic/TopicService.java
@@ -0,0 +1,51 @@
+package io.javabrains.springbootstarter.topic;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+// Spring Business Service
+
+@Service
+public class TopicService {
+	
+	List<Topic> topics = new ArrayList<>(Arrays.asList(
+			new Topic("spring", "Spring boot name", "Spring boot description"),
+			new Topic("java", "java name", "java description"),
+			new Topic("SQL", "SQL name", "SQL description")
+		));
+	
+	public List<Topic> getAllTopics(){
+		return topics;
+	}
+	
+	public Topic getTopic(String id) {
+		return topics.stream().filter(t -> t.getId().equals(id)).findFirst().get();
+	}
+	
+	public void addTopic(Topic topic) {
+		topics.add(topic);
+	}
+
+	public void updateTopic(String id, Topic topic) {
+		for (int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if (t.getId().equals(id)) {
+				topics.set(i, topic);
+				return;
+			}
+		}
+	}
+
+	public void deleteTopic(String id) {
+		/*for (int i = 0; i < topics.size(); i++) {
+			Topic t = topics.get(i);
+			if (t.getId().equals(id)) {
+				topics.remove(i);
+				return;
+			}
+		}*/
+		topics.removeIf(t -> t.getId().equals(id));
+	}
+}
